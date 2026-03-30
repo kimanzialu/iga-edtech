25c9f1aec13d0290968f385c05d8557d5a6e0e07
IYKenny <imanziyannickkenny@gmail.com>
2026-03-30 15:50:33 +0200

    Update api

--- iga_edtech_frontend/js/api.js
+++ iga_edtech_frontend/js/api.js
@@ -1,5 +1,5 @@
 
-const API_BASE = 'https://iga-edtech-backend.onrender.com/auth';
+const API_BASE = 'https://iga-edtech-backend.onrender.com';
 
 const Auth = {
   getToken()       { return localStorage.getItem('iga_access_token'); },
